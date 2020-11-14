b6f5ea4177c69fbf704d0ad7dcb530a480ff5280
Marcos J Ribeiro <mjribeiro@usp.br>
2020-11-14 12:11:15 -0300

    atualizei a pasta

--- ceper_rais/rais.R
+++ ceper_rais/rais.R
@@ -15,7 +15,6 @@ rm(polygons_regioes_gov_sp)
 
 
 
-tm_shape(polygons_regioes_gov_sp) + tm_polygons()
 
 
 ## dados da rais
@@ -26,12 +25,21 @@ idade = data.frame(idade)
 
 
 
-head(idade)
+
+
+region$var = idade[,2]
 
 
 
 
-
+  tm_shape(region) +
+  tm_polygons('var',  textNA = 'Sem dados') +
+  tm_compass(type = "8star", position = c("right", "bottom", size = 0.0)) +
+  tm_scale_bar(breaks = c(0, 100, 200, 300), text.size = 0.6) +
+  tm_layout(
+    legend.text.size = 0.8,
+    frame = T,
+    legend.format = list(text.separator = "-"))
 
 
 
