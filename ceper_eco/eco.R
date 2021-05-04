b839a6fe644eae9c80a37eba9d0461e0bd8e434f
Marcos J Ribeiro <mjribeiro@usp.br>
2021-05-04 17:37:34 -0300

    update

--- ceper_eco/eco.R
+++ ceper_eco/eco.R
@@ -18,7 +18,7 @@ library(raster)
 library(tmap)
 library(reshape2)
 library(writexl)
-
+library(viridis)
 
 
 
@@ -35,6 +35,119 @@ sp = sp[order(sp$name_muni), ]
 # Dados Alesp ----
 
 df = read_excel('muni.xlsx', sheet = 'data_18')
+
+
+# PCA nos dados ----
+
+x = df[,c('riqueza', 'longevidade', 'escolaridade')]
+
+pca = prcomp(x)
+
+pca = princomp(x, cor = T, scores = TRUE)
+
+summary(pca)
+
+
+
+df['pca'] = pca$scores[,1]
+
+
+test = df[order(df$pca, decreasing = T), 
+          c( 'name_muni', 'grupo', 'pca', "escolaridade", 'riqueza', 'longevidade')]
+
+
+# dotchart 1
+
+
+par(family = "mono", font=2)  # mudar a fonte
+
+options(OutDec= ",")         # colocar o separador decimal sendo vírgula
+
+
+
+
+melhor = df[order(df$pca, decreasing = T), 
+   c( 'name_muni', 'pca', 'grupo')][1:10,]
+
+melhor = melhor[order(melhor$pca, decreasing = F), ]
+
+
+melhor = data.frame(melhor)
+
+
+
+comp0 = max( nchar(melhor$name_muni) ) - nchar(melhor$name_muni) 
+
+
+
+for (i in 1:length(comp0)) {
+  melhor[i, 'concat'] = paste(melhor$name_muni[i], '-', sep = strrep('-',comp0[i] ) )
+}
+
+
+melhor['concat2'] = paste(melhor$concat, melhor$grupo, sep= '')
+
+
+
+jpeg("dot1.jpg", width = 600,height = 400)
+
+par(family = "mono", font=2)  # mudar a fonte
+
+dotchart(melhor$pca, 
+         melhor$concat2, 
+         pch=19, 
+         pt.cex = 2,
+         xlab = 'PCA')
+title('Municípios e Grupos do IPRS', outer = T, adj=0.05, line=-2.5)
+
+
+dev.off()
+
+
+
+
+
+
+# dotchart 2 
+
+
+pior = df[order(df$pca, decreasing = T), 
+   c( 'name_muni', 'grupo', 'pca', "escolaridade", 'riqueza', 'longevidade')][(n-9):n,]
+
+pior = data.frame(pior)
+
+
+
+comp = max( nchar(pior$name_muni) ) - nchar(pior$name_muni) 
+
+
+
+for (i in 1:length(comp)) {
+  pior[i, 'concat'] = paste(pior$name_muni[i], '-', sep = strrep('-',comp[i] ) )
+}
+
+
+pior['concat2'] = paste(pior$concat, pior$grupo, sep= '')
+
+
+jpeg("dot2.jpg", width = 600,height = 400)
+
+par(family = "mono", font=2)  # mudar a fonte
+
+dotchart(pior$pca, 
+         pior$concat2, 
+         pch=21,
+         #color = 'darkred',
+         bg = 'darkred',
+         pt.cex = 2,
+         xlab = 'PCA')
+title('Municípios e Grupos do IPRS', outer = T, adj=0.05, line=-2.5)
+dev.off()
+
+
+
+pior
+
 
 
 # Merge data ----
@@ -54,16 +167,14 @@ df2[df2$name_muni.x != df2$name_muni.y, c('name_muni.x','name_muni.y')]
 
 
 
-maps_f = function(sh, x, leg, fonte){
+maps_f = function(sh, x, leg, fonte, breaks, labels, colors){
   g1 =  tm_shape(sh) +
     tm_polygons(x,  
-                title=toupper(leg),  
+                title=leg,  
                 textNA = 'Sem dados',
-                style="pretty",
-                palette="PuOr"
-               # breaks = mybreaks,
-                #label = mylabel,
-                #palette='Reds'
+                breaks = breaks,
+                label = labels,
+                palette=colors
                ) +
     tm_compass(type = "8star",
                position = c("right", "bottom", size = 0.0)) +
@@ -72,35 +183,92 @@ maps_f = function(sh, x, leg, fonte){
       legend.text.size = 0.8,
       frame = T,
       legend.format = list(text.separator = "-"))
-#  nam =  paste("G_", sep = "")
-#  assign(nam, g1)
-  tmap_save(g1, filename = paste(fonte,leg,".png", sep = ""), width = 12,
-            height = 6, units = 'in')
+  tmap_save(g1, filename = paste(fonte,leg,".eps", sep = ""), width = 6,
+            height = 4, units = 'in')
 }
 
 
 
-
-maps_f(df2,'riqueza','Riqueza', 'Alesp_')
-
+# Longevidade ----
 
 
 
-mybreaks = seq(0, 100, 10)
+mybreaks2 = c(seq(50, 90, 10), Inf)
 
-mylabel = c('0% a 10%',
-            '10% a 20%',
-            '20% a 30%',
-            '30% a 40%',
-            '40% a 50%',
-            '50% a 60%',
-            '60% a 70%',
-            '70% a 80%',
-            '80% a 90%',
-            '90% a 100%'
-)
+mylabel2 = c('50 a 60',
+             '60 a 70',
+             '70 a 80',
+             '80 a 90',
+             'Mais de 90')
 
-mycolors = c('beige','burlywood1','orange','darkorange1', 'darkorange3')
+
+
+mycolors2 = inferno(length(mylabel2), alpha = 1, begin = 0.15, end = 1, direction = 1)
+
+
+
+maps_f(sh=df2, x='longevidade',
+       leg='Longevidade', 
+       fonte='A_',
+       breaks = mybreaks2,
+       labels = mylabel2,
+       colors = mycolors2)
+
+
+
+
+# Riqueza ----
+
+mybreaks = c(seq(10, 60, 10), Inf)
+
+mylabel = c(
+            '10 a 20',
+            '20 a 30',
+            '30 a 40',
+            '40 a 50',
+            '50 a 60',
+            'Mais de 60')
+
+
+mycolors = inferno(length(mylabel), alpha = 1, begin = 0.15, end = 1, direction = 1)
+
+
+maps_f(sh=df2, x='riqueza',
+       leg='Riqueza', 
+       fonte='A_',
+       breaks = mybreaks,
+       labels = mylabel,
+       colors = mycolors)
+
+
+
+
+
+# Escolaridade ----
+
+
+
+
+mybreaks3 = c(seq(30, 70, 10), Inf)
+
+mylabel3 = c('30 a 40',
+            '40 a 50',
+            '50 a 60',
+            '60 a 70',
+            'Mais de 70')
+
+
+mycolors3 = inferno(length(mylabel3), alpha = 1, begin = 0.15, end = 1, direction = 1)
+
+
+maps_f(sh=df2, x='escolaridade',
+       leg='Escolaridade', 
+       fonte='A_',
+       breaks = mybreaks3,
+       labels = mylabel3,
+       colors = mycolors3)
+
+
 
 
 
